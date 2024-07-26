5009d528df430116a280cf816d0fd08085ee0247
SpidFightFR <spidfight@swisscows.email>
2024-07-26 17:19:23 +0200

    Moved some config location for cleaner codebase
    Signed-off-by: SpidFightFR <spidfight@swisscows.email>

--- nix-cfgs/env.nix
+++ nix-cfgs/env.nix
@@ -6,7 +6,11 @@
     experimental-features = nix-command flakes
   '';
 
+  #Equivalent of /etc/environment
   environment.sessionVariables = {
     EDITOR = "nvim";
   };
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
 }
--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -36,9 +36,6 @@
     fastfetch
   ];
 
-  # Backup etc files instead of failing to activate generation if a file already exists in /etc
-  environment.etcBackupExtension = ".bak";
-
   # Read the changelog before changing this value
   system.stateVersion = "24.05";
 
