b632be0abe1352351e32a7e1d85efa34802e92c1
SpidFightFR <spidfight@swisscows.email>
2024-12-28 18:21:28 +0100

    [FIX] - Fixed outputs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
   };
 
-  outputs = { nixpkgs, nix-on-droid }: {
+  outputs = { self, nixpkgs, home-manager, nix-on-droid }: {
 
     nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
       pkgs = import nixpkgs { system = "aarch64-linux"; };
