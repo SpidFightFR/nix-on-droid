1394751e0979e691c91c845bbc5b518666e226f0
SpidFightFR <spidfight@swisscows.email>
2025-10-15 15:52:33 +0200

    [UPDATE] - Updated flake.lock 15/10/25

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Basic example of Nix-on-Droid system config.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     home-manager = {
-          url = "github:nix-community/home-manager/release-24.11";
+          url = "github:nix-community/home-manager/release-25.05";
           inputs.nixpkgs.follows = "nixpkgs";
     };
 
